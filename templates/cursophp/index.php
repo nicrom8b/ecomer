0aa05e497c1ec87073643e6c126e0615cef98ff5
Jorge J. Riera <jorge.riera9@gmail.com>
2013-07-27 23:57:56 -0300

    realizando modificaciones para que tome bien el responsivo - Realice pruebas desde smpartphone

--- templates/cursophp/index.php
+++ templates/cursophp/index.php
@@ -4,21 +4,16 @@ defined('_JEXEC') or die;
 $app = JFactory::getApplication();
 ?>
 <!DOCTYPE html>
- <meta charset="utf-8">
+<html lang="es">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
  <jdoc:include type="head" />
 <link rel="stylesheet" type="text/css" href="templates/cursophp/bootstrap/css/bootstrap.css">   
 <link href="templates/cursophp/bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
 
 </head>
   <body >
-
-
-    <div class="row-fluid">
-      <div class="hero-unit"  style="background-image:url(images/fondo-banner.jpg)">
-        <h1>Ecommer</h1>
-        <h3>Compra y venta</h3> 
-      </div>
-    </div>
   
 
   	<div class="navbar navbar-inverse navbar-fixed-top"> 
@@ -31,13 +26,17 @@ $app = JFactory::getApplication();
 	        </div>
 		  </div>
    </div>
+   
+   <div class="row-fluid">
+      <div class="hero-unit"  style="background-image:url(images/fondo-banner.jpg)">
+        <h1>Ecommer Jujuy</h1>
+        <h3>Compra y venta</h3> 
+      </div>
+    </div>
 
   <div class="container-fluid">
     <div class="row-fluid">
-      
-     
- 
-    <div class="span3">
+      <div class="span3">
       
         <div class="well sidebar-nav">
         <jdoc:include type="modules" name="categoria" style="none" />
@@ -54,7 +53,7 @@ $app = JFactory::getApplication();
           <jdoc:include type="component" name='publicofinal' />
         </div>
       </div><!--/row-->
-    <!-- </div><!--/span--> 
+    <!-- </div><!/span--> 
 
    </div><!--/row-->
   </div><!--/.fluid-container-->
